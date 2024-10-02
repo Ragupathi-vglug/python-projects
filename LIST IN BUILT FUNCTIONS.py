6183224cc804c1132700fed11bc8b0a2f09b949b
RAGU_VGLUG <ragu26555@gmail.com>
2024-10-02 07:59:21 -0700

    LIST IN BUILT FUNCTIONS.py

--- LIST IN BUILT FUNCTIONS.py
+++ LIST IN BUILT FUNCTIONS.py
@@ -1,1 +1,30 @@
 # LIST IN BUILT FUNCTIONS
+# LIST IN BUILT FUNCTIONS
+a=[1,2,3,4]
+a.append(5)     
+print(a)
+a.remove(1)
+print(a)
+a.insert(1,2)
+print(a)
+print(a.count(2))
+print(a.index(4))
+print(max(a))
+print(min(a))
+print(len(a))
+a.remove(2)
+print(a)
+a.pop(0)
+print(a)
+a.extend("q")
+print(a)
+print(type(a))
+
+#LIST CONSRUCTOR
+print(list(range(10)))
+print(list("Kowshik"))
+b=[6,25,73,31,1,4547]
+b.sort()
+print(b)
+b.sort(reverse=True)
+print(b)
